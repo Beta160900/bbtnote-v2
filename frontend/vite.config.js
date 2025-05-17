4dd30c70c5bca5136acaf6c628bb0e52bb3ebc24
beta160900 <beta010js@gmail.com>
2025-05-17 10:47:04 +0700

    add some redirect in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { writeFileSync } from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    {
+      name: 'generate-redirects',
+      closeBundle() {
+        // Create a _redirects file in the dist directory
+        writeFileSync('dist/_redirects', '/* /index.html 200');
+      }
+    }
   ],
 })
