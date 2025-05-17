fcbfe96c1619b6755bc4db4a7a725e5e16afa02c
beta160900 <beta010js@gmail.com>
2025-05-17 11:06:22 +0700

    remove vite generate redirect

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { writeFileSync } from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    {
-      name: 'generate-redirects',
-      closeBundle() {
-        // Create a _redirects file in the dist directory
-        writeFileSync('dist/_redirects', '/* /index.html 200');
-      }
-    }
   ],
 })
